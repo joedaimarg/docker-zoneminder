96e58b0e9891ce956c8e8f87484a7c6d06ecd2b6
Joedaimar G <joedaimar@stratiumsoftware.com>
2020-10-14 14:22:31 +0800

    Prepare zmhdeb build image

--- build-zmh.sh
+++ build-zmh.sh
@@ -2,24 +2,29 @@
 # NOTE: make sure to chmod a+x build-zmh.sh
 
 # Installer script name
-FILE=do_debian_package.sh
+#FILE=do_debian_package.sh
 
-if test -f "$FILE"; then
-  echo "$FILE exists. Script will now continue."
-else 
+#if test -f "$FILE"; then
+#  echo "$FILE exists. Script will now continue."
+#else 
   # Grab the package installer script
-  wget https://raw.githubusercontent.com/joedaimarg/zoneminder/zmh/utils/$FILE
-  chmod a+x $FILE
-fi
+  #wget https://raw.githubusercontent.com/joedaimarg/zoneminder/zmh/utils/$FILE
+  #chmod a+x $FILE
+#fi
 
 # Update the system
-sudo apt-get update
+#sudo apt-get update
 
 # Build the latest master snapshot
-./$FILE --snapshot=NOW --branch=zmh-dev --type=fork --fork=joedaimarg --interactive=no
+#./$FILE --snapshot=NOW --branch=zmh-dev --type=fork --fork=joedaimarg --interactive=no
+
+# Build installer
+docker build --build-arg PASS_CACHE="$(date)" -t zmhdeb -f BuildsystemFocal .
+docker-compose -f zmhdeb-compose.yaml up -d
+docker-compose -f zmhdeb-compose.yaml down
 
 # Build image
-docker build --build-arg PASS_CACHE="$(date)" -t tbmzmh -f Dockerfile .
+#docker build --build-arg PASS_CACHE="$(date)" -t tbmzmh -f Dockerfile .
 
 # Clean up build
-./do_debian_package.sh clean && rm -rf zoneminder_* && rm -rf do_debian_package.sh* && rm -rf zoneminder-d*
+#rm -rf zoneminder_* && rm -rf do_debian_package.sh* && rm -rf zoneminder-d*
